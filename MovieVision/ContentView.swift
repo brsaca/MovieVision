7df566341bea825ef2dd7d49e36545d294a0d840
brsaca <brsaca@gmail.com>
2024-01-24 16:49:15 -0600

    Initial Commit

---
+++ MovieVision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MovieVision
+//
+//  Created by Brenda Saavedra Cantu on 24/01/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ MovieVision/MovieVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieVisionApp.swift
+//  MovieVision
+//
+//  Created by Brenda Saavedra Cantu on 24/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
